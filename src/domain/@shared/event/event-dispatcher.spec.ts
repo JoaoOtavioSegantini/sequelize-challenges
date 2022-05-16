03297bcc89cfe5b1b2748775b1aead06154be822
João Otávio <joaotavioos@outlook.com>
2022-05-16 14:20:42 -0300

    feat: finish Eventos de Customer challenge ✨

--- src/domain/@shared/event/event-dispatcher.spec.ts
+++ src/domain/@shared/event/event-dispatcher.spec.ts
@@ -1,6 +1,10 @@
 import SendEmailWhenProductIsCreatedHandler from "../../product/event/handler/send-email-when-product-is-created.handler";
+import { SendEmailWhenCustomerIsCreatedHandler1, SendEmailWhenCustomerIsCreatedHandler2 } from "../../customer/event/handler/send-email-when-customer-is-created.handler";
 import ProductCreatedEvent from "../../product/event/product-created.event";
+import CustomerCreatedEvent from "../../customer/event/customer-created.event";
 import EventDispatcher from "./event-dispatcher";
+import SendEmailWhenCustomerChangeAddress from "../../customer/event/handler/send-email-when-customer-change-address.handler";
+import CustomerChangeAddressEvent from "../../customer/event/customer-change-address.event";
 
 describe("Domain events tests", () => {
   it("should register an event handler", () => {
@@ -78,5 +82,70 @@ describe("Domain events tests", () => {
     eventDispatcher.notify(productCreatedEvent);
 
     expect(spyEventHandler).toHaveBeenCalled();
+
+  });
+  it("should unregister all event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]
+    ).toMatchObject(eventHandler);
+
+    eventDispatcher.unregisterAll();
+
+    expect(
+      eventDispatcher.getEventHandlers["ProductCreatedEvent"]
+    ).toBeUndefined();
+  });
+
+  it("should notify all event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenCustomerIsCreatedHandler1();
+    const eventHandler2 = new SendEmailWhenCustomerIsCreatedHandler2();
+    const eventHandler3 = new SendEmailWhenCustomerChangeAddress();
+
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+    const spyEventHandler2 = jest.spyOn(eventHandler2, "handle");
+    const spyEventHandler3 = jest.spyOn(eventHandler3, "handle");
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+    eventDispatcher.register("CustomerChangeAddressEvent", eventHandler3);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]
+    ).toMatchObject(eventHandler2);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerChangeAddressEvent"][0]
+    ).toMatchObject(eventHandler3);
+
+    const customerCreatedEvent = new CustomerCreatedEvent({
+      name: "Product 1",
+      description: "Product 1 description",
+      price: 10.0,
+    });
+
+    const customerChangeAddressEvent = new CustomerChangeAddressEvent({
+      name: "John Doe",
+      id: "1",
+      address: "Rua José Nascimento de Almeida, nº 500"
+    });
+
+    // Quando o notify for executado o SendEmailWhenProductIsCreatedHandler.handle() deve ser chamado
+    eventDispatcher.notify(customerCreatedEvent);
+    eventDispatcher.notify(customerChangeAddressEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
+    expect(spyEventHandler2).toHaveBeenCalled();
+    expect(spyEventHandler3).toHaveBeenCalled();
+
   });
 });
---
+++ src/domain/customer/event/handler/send-email-when-customer-is-created.handler.ts
@@ -0,0 +1,19 @@
+import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
+import CustomerCreatedEvent from "../customer-created.event";
+
+export class SendEmailWhenCustomerIsCreatedHandler1
+  implements EventHandlerInterface<CustomerCreatedEvent>
+{
+  handle(event: CustomerCreatedEvent): void {
+    console.log(`Esse é o primeiro console.log do evento: CustomerCreated`);
+  }
+}
+
+export class SendEmailWhenCustomerIsCreatedHandler2
+  implements EventHandlerInterface<CustomerCreatedEvent>
+{
+  handle(event: CustomerCreatedEvent): void {
+    console.log(`Esse é o segundo console.log do evento: CustomerCreated`);
+  }
+}
+
