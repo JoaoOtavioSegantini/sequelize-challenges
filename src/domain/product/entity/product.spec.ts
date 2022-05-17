498c883b5b927cfacd985e375e2dbb7145cc46b6
João Otávio <joaotavioos@outlook.com>
2022-05-17 12:20:19 -0300

    feat: update product unit test  ✨

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -19,6 +19,14 @@ describe("Product unit tests", () => {
     }).toThrowError("product: Price must be greater than zero");
   });
 
+  it("should throw error when name is empty and price is less than zero", () => {
+    expect(() => {
+      const product = new Product("123", "", -100);
+    }).toThrowError(
+      "product: Name is required,product: Price must be greater than zero"
+    );
+  });
+
   it("should change name", () => {
     const product = new Product("123", "Product 1", 100);
     product.changeName("Product 2");
