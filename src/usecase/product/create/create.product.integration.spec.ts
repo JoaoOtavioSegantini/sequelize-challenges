9d340364ff24960a81b246df9c1a34dd41300765
João Otávio <joaotavioos@outlook.com>
2022-05-17 12:20:44 -0300

    feat: add product integration tests  ✨

---
+++ src/usecase/product/create/create.product.integration.spec.ts
@@ -0,0 +1,49 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import CreateProductUseCase from "./create.product.usecase";
+
+describe("Test create product use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a product", async () => {
+    const productRepository = new ProductRepository();
+    const usecase = new CreateProductUseCase(productRepository);
+
+    const product = ProductFactory.create("a", "Product 1", 50);
+
+    await productRepository.create(product as any);
+
+    const input = {
+      name: product.name,
+      price: product.price,
+    };
+
+    const result = await usecase.execute(input);
+
+    const output = {
+      id: result.id,
+      name: "Product 1",
+      price: 50,
+    };
+
+    expect(result).toEqual(output);
+  });
+});
---
+++ src/usecase/product/list/list.product.integration.spec.ts
@@ -0,0 +1,49 @@
+import { Sequelize } from "sequelize-typescript";
+import Product from "../../../domain/product/entity/product";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import ListProductUseCase from "./list.product.usecase";
+
+describe("Test list product use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should list a product", async () => {
+    const productRepository = new ProductRepository();
+    const usecase = new ListProductUseCase(productRepository);
+
+    const product = new Product("123", "Product 1", 50);
+    const product2 = new Product("456", "Product 2", 10);
+
+    await productRepository.create(product);
+    await productRepository.create(product2);
+
+    const input = {};
+
+    const output = {
+      products: [
+        { id: product.id, name: product.name, price: product.price },
+        { id: product2.id, name: product2.name, price: product2.price },
+      ],
+    };
+
+    const result = await usecase.execute(input);
+
+    expect(result).toEqual(output);
+  });
+});
